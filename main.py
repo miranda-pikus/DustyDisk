2dab08d7ca8d8bdc613c2731cd16d5848c2e8b0c
Miranda Pikus <mpikus@purdue.edu>
2025-08-05 15:15:54 -0500

    added Grid class, initialize system, constants, plotting, and the first example in tutorial notebook

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 import numpy as np
+import Constants 
 
 class Grid():
     '''
     class that represents the grid that gets initialized by the user with
     an input radius, gas density, and gas temperature profile
     '''
-    def __init__(self, radius, sigma_gas, Tgas, unit_type):
-        self.radius = radius # array of radius
-        self.sigma_gas = sigma_gas # array of gas density
-        self.Tgas = Tgas # array of gas temperature 
-        self.unit_type = unit_type # unit system (string)
-        self.Pressure = np.zeros(len(radius))
-        # integrate different units here eventually, for now assume cgs
+    def __init__(self, radius, sigma_gas, Tgas, mu_gas, Mstar, unit_type):
+        '''
+        Args: 
+            radius (array) : radial profile
+            sigma_gas (array) : gas density
+            Tgas (array) : gas temperature
+            mu_gas (float) : mean molecular weight of gas
+            Mstar (float) : mass of central star
+            unit_type (string) : unit system (cgs everywhere for now)
+        '''
+        self.radius = radius 
+        self.sigma_gas = sigma_gas 
+        self.Tgas = Tgas 
+        #self.unit_type = unit_type # unit system (string)
 
-    #def CalculatePressureGrid(self):
+        # sound speed
+        Cs = np.sqrt(Constants.k_B * Tgas * (radius/Constants.AU))**(-0.5) / (mu_gas*Constants.m_H) # cm/s
+        # keplerian angular velocity
+        Omega_K = np.sqrt(Constants.G * Mstar / radius**3)
+        # keplerian velocity
+        v_K = Omega_K * radius
+        # pressure
+        self.Pressure = sigma_gas * Cs * Omega_K / np.sqrt(2*np.pi)
 
 
-
-
-def Initialize_System(radius, sigma_gas, Tgas, unit_type='cgs'):
+def Initialize_System(radius, sigma_gas, Tgas, mu_gas, Mstar, unit_type='cgs'):
     '''
     Args: 
         radius (array of floats) -- radius distribution
@@ -26,9 +39,5 @@ def Initialize_System(radius, sigma_gas, Tgas, unit_type='cgs'):
         Tgas (array of floats) -- gas temperature 
     Returns: theGrid (Grid) with user-input radius, sigma_gas, Tgas
     '''
-    if len(radius)*len(sigma_gas)*len(Tgas)/3 != len(radius):
-        print('cannot initialize system, input arrays are different lengths! try again.')
-        return 0.
-    else: 
-        theGrid = Grid(radius, sigma_gas, Tgas, unit_type)
-        return theGrid
+    theGrid = Grid(radius, sigma_gas, Tgas, mu_gas, Mstar, unit_type)
+    return theGrid
