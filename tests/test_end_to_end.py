e44ee24556527dcd6c84133f0f5048de7a7216fe
Miranda Pikus <mpikus@purdue.edu>
2025-08-07 15:19:51 -0500

    added test files

---
+++ tests/test_end_to_end.py
@@ -0,0 +1,9 @@
+import numpy as np
+from DustyDisk.Functions import *
+from DustyDisk.Constants import AU as au
+
+def test_constant_pressure():
+    '''
+    checks the calculation of vdrift and density for dust given a constant pressure 
+    '''
+
